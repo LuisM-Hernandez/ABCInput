4aeccdc9464fb98350d381b0bf74cfb576ea449f
Luis <m.luishernandez@outlook.com>
2020-10-27 03:38:22 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABCInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select a letter between A,B,C or Q");
+        }
+    }
+}
